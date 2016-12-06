6308d6a375d3af84e28ddf4d500465ff9bdf13d3
geine <bonos2@wisdo.me>
2016-12-07 02:05:38 +0900

    Add receiveNumberList for multi send sms

--- magpie-web/src/main/java/com/tcp/geine/vo/Sms.java
+++ magpie-web/src/main/java/com/tcp/geine/vo/Sms.java
@@ -1,25 +1,28 @@
 package com.tcp.geine.vo;
 
 import java.util.Date;
+import java.util.List;
 
 public class Sms {
 
 	private String applicationName;
 	private String receiveNumber;
+	private List<String> receiveNumberList;
 	private String content;
 	private Date reserveDate;
 	private String sendNumber;
 	
-	public Sms(String applicationName, String receiveNumber, String content, Date reserveDate, String sendNumber) {
+	public Sms(String applicationName, String receiveNumber, String content, Date reserveDate, String sendNumber, List<String> receiveNumberList) {
 		
 		if (applicationName == null) throw new NullPointerException("applicationName is not null");
-		if (receiveNumber == null) throw new NullPointerException("receiveNumber is not null");
+		if ((receiveNumber == null) && (receiveNumberList == null)) throw new NullPointerException("receiveNumber is not null");
 		
 		this.applicationName = applicationName;
 		this.receiveNumber = receiveNumber;
 		this.content = content;
 		this.reserveDate = reserveDate;
 		this.sendNumber = sendNumber;
+		this.receiveNumberList = receiveNumberList;
 	}
 	
 	public Sms(){
@@ -65,11 +68,19 @@ public class Sms {
 	public void setSendNumber(String sendNumber) {
 		this.sendNumber = sendNumber;
 	}
+	
+	public List<String> getReceiveNumberList() {
+		return receiveNumberList;
+	}
+
+	public void setReceiveNumberList(List<String> receiveNumberList) {
+		this.receiveNumberList = receiveNumberList;
+	}
 
 	@Override
 	public String toString() {
 		return "Sms [applicationName=" + applicationName + ", receiveNumber=" + receiveNumber + ", content=" + content
-				+ ", reserveDate=" + reserveDate + ", sendNumber=" + sendNumber + "]";
+				+ ", reserveDate=" + reserveDate + ", sendNumber=" + sendNumber + ", receiveNumberList" + receiveNumberList + "]";
 	}
 	
 }
